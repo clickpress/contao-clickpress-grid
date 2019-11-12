47a2c406ca2c85bda4575849378be8318ce0f51c
stefansl <ssl@clickpress.de>
2019-11-12 14:13:49 +0100

    Fix docblock

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,15 +1,14 @@
 <?php
+
 /*
- * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
+ * This file is part of Contao Clickpress Grid.
+ *
+ * (c) Stefan Schulz-Lauterbach (https://clickpress.de)
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-/**
- * RockSolid Columns configuration
- *
- * @author Martin Auswöger <martin@madeyourday.net>
- */
+
 
 $GLOBALS['TL_CTE']['cp_grid']['cp_grid_start'] = 'Clickpress\\ContaoClickpressGridBundle\\Element\\GridStart';
 $GLOBALS['TL_CTE']['cp_grid']['cp_grid_stop'] = 'Clickpress\\ContaoClickpressGridBundle\\Element\\GridEnd';
