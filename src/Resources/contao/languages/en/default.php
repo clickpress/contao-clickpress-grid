d087f2c57f4168023c5134f1dfc498a231e3cc25
stefansl <ssl@clickpress.de>
2019-11-14 17:33:53 +0100

    Add en translation

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,11 @@
+<?php
+
+$GLOBALS['TL_LANG']['CTE']['cp_grid'] = 'Grid Spaltenset';
+$GLOBALS['TL_LANG']['CTE']['cp_grid_start'][0] = 'Gridset start';
+$GLOBALS['TL_LANG']['CTE']['cp_grid_start'][1] = '';
+$GLOBALS['TL_LANG']['CTE']['cp_grid_stop'][0] = 'Gridset stop';
+$GLOBALS['TL_LANG']['CTE']['cp_grid_stop'][1] = '';
+$GLOBALS['TL_LANG']['CTE']['cp_column_start'][0] = 'Column start';
+$GLOBALS['TL_LANG']['CTE']['cp_column_start'][1] = '';
+$GLOBALS['TL_LANG']['CTE']['cp_column_stop'][0] = 'Column stop';
+$GLOBALS['TL_LANG']['CTE']['cp_column_stop'][1] = '';
---
+++ src/Resources/contao/languages/en/tl_content.php
@@ -0,0 +1,29 @@
+<?php
+$GLOBALS['TL_LANG']['tl_content']['cp_grid_desktop'] = ['Desktop', 'Set columns in desktop view'];
+$GLOBALS['TL_LANG']['tl_content']['cp_grid_tablet'] = ['Tablet', 'Set columns in tablet view'];
+$GLOBALS['TL_LANG']['tl_content']['cp_grid_mobile'] = ['Mobile', 'Set columns in mobile view'];
+
+// References
+$GLOBALS['TL_LANG']['tl_content']['cp_grid_options'] = [
+    'grid_100' => '1 column',
+    'grid_50_50' => '2 columns',
+    'grid_33_33_33' => '3 columns',
+    'grid_25_25_25_25' => '4 columns',
+    'grid_75_25' => '2 columns - divided into 75% 25%',
+    'grid_25_75' => '2 columns - divided into 25% 75%',
+    'grid_66_33' => '2 columns - divided into 66% 33%',
+    'grid_33_66' => '2 columns - divided into 33% 66%',
+    'grid_50_25_25' => '3 columns - divided into 50% 25% 25%',
+    'grid_25_50_25' => '3 columns - divided into 25% 50% 25%',
+    'grid_25_25_50' => '3 columns - divided into 25% 25% 50%',
+    'grid_40_30_30' => '3 columns - divided into 40% 30% 30%',
+    'grid_30_40_30' => '3 columns - divided into 30% 40% 30%',
+    'grid_30_30_40' => '3 columns - divided into 30% 30% 40%',
+    'grid_40_20_20_20' => '4 columns - divided into 40% 20% 20% 20%',
+    'grid_20_40_20_20' => '4 columns - divided into 20% 40% 20% 20%',
+    'grid_20_20_40_20' => '4 columns - divided into 20% 20% 40% 20%',
+    'grid_20_20_20_40' => '4 columns - divided into 20% 20% 20% 40%',
+];
+
+// Legend
+$GLOBALS['TL_LANG']['tl_content']['cp_grid_legend'] = 'Column configuration';
