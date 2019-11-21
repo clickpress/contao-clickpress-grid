e29c846090bce35c17742b1f8df062d69592a70e
stefansl <ssl@clickpress.de>
2019-11-21 13:45:58 +0100

    Add english translation in page layout

---
+++ src/Resources/contao/languages/de/tl_layout.php
@@ -0,0 +1,7 @@
+<?php
+// Legends
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_legend'] = 'Clickpress Grid CSS';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][0] = 'Clickpress Grid CSS laden';
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][1] = 'Das vordefinierte CSS dem Seitenlaout hinzufügen. Alternativ kann auch ein eigenes CSS eingebunden werden.';
---
+++ src/Resources/contao/languages/en/tl_layout.php
@@ -0,0 +1,7 @@
+<?php
+// Legends
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_legend'] = 'Clickpress Grid CSS';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][0] = 'Load Clickpress Grid CSS';
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][1] = 'Add the predefined Grid to this page layout. Uncheck this checkbox, if you want to integrate your own Grid CSS.';
