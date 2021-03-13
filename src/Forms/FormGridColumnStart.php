78ddf5738b0a21bc00337b144f0a5747ef6059cf
Jannik <mail@jaynoe.de>
2021-03-13 14:28:32 +0100

    Add grid-layout to form generator

---
+++ src/Forms/FormGridColumnStart.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of Contao Clickpress Grid.
+ *
+ * (c) Stefan Schulz-Lauterbach (https://clickpress.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clickpress\ContaoClickpressGridBundle\Forms;
+
+use Psr\Log\LogLevel;
+use Contao\CoreBundle\Monolog\ContaoContext;
+
+/**
+ * Column start content element
+ * Taken with friendly permission from RockSolid Columns.
+ *
+ * @author Martin Auswöger <martin@madeyourday.net>
+ * @author Stefan Schulz-Lauterbach <ssl@clickpress.de>
+ * @author Jannik Nölke <mail@jaynoe.de>
+ */
+class FormGridColumnStart extends \Widget
+{
+    /**
+     * @var string Template
+     */
+    protected $strTemplate = 'form_grid_column_start';
+
+    /**
+     * Do not validate
+     */
+    public function validate()
+    {
+    }
+
+    /**
+     * Parse the template file and return it as string
+     *
+     * @param array $arrAttributes An optional attributes array
+     *
+     * @return string The template markup
+     */
+    public function parse($arrAttributes=null)
+    {
+        if (TL_MODE === 'BE') {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            return $objTemplate->parse();
+        }
+
+        return parent::parse($arrAttributes);
+    }
+
+    /**
+     * Generate the widget and return it as string
+     *
+     * @return string The widget markup
+     */
+    public function generate()
+    {
+        return "";
+    }
+}
