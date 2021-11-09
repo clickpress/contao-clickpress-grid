f76e4087665ad18d5af7b632753c8b4a542d2f63
stefansl <ssl@clickpress.de>
2021-11-09 14:11:53 +0100

    Fix typo

--- src/Resources/contao/languages/de/tl_layout.php
+++ src/Resources/contao/languages/de/tl_layout.php
@@ -4,4 +4,4 @@ $GLOBALS['TL_LANG']['tl_layout']['cp_grid_legend'] = 'Clickpress Grid CSS';
 
 // Fields
 $GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][0] = 'Clickpress Grid CSS laden';
-$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][1] = 'Das vordefinierte CSS dem Seitenlaout hinzufügen. Alternativ kann auch ein eigenes CSS eingebunden werden.';
+$GLOBALS['TL_LANG']['tl_layout']['cp_grid_load_css'][1] = 'Das vordefinierte CSS dem Seitenlayout hinzufügen. Alternativ kann auch ein eigenes CSS eingebunden werden.';
